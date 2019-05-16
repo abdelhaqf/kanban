9a6458d0d94b958b3fd76d1a3ead6ca2727ad49e
abdelhaqf <abdel_31354@yahoo.co.id>
2019-05-16 10:59:31 +0700

    optimasi code login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,42 @@
 <template lang="html">
   <div class="login">
+
     <div class="container">
+
       <a id="title">Login</a>
-      <input v-model="user.uname" ref="uname" @keydown.enter="$refs.pass.focus()" placeholder="username">
-      <input v-model="user.pass" ref="pass" @keydown.enter="login" type="password" placeholder="password">
+
+      <input
+        v-model="user.uname"
+        ref="uname"
+        @keydown.enter="$refs.pass.focus()"
+        placeholder="username">
+
+      <input
+        v-model="user.pass"
+        ref="pass"
+        @keydown.enter="login"
+        type="password"
+        placeholder="password">
+
+      <a id="tips">Tekan <b>[ENTER]</b> untuk masuk</a>
     </div>
+
   </div>
 </template>
 
 <script>
 import * as dss from 'deepstream.io-client-js'
+
 export default {
+
   name: 'login',
+
   mounted() {
     this.$refs.uname.focus()
   },
+
   data: function() {
     return {
-      // ds: dss('localhost:6020'),
       user: {
         uname: '',
         pass: '',
@@ -25,46 +44,63 @@ export default {
       }
     }
   },
+
   methods: {
     login() {
       this.$parent.login(this.user.uname, this.user.pass)
     }
   }
+
 }
 </script>
 
 <style lang="scss" scoped>
+
 .login {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  width: 100%;
   position: fixed;
   top: 0;
   left: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 100%;
+  width: 100%;
 }
+
 .container {
   color: grey;
   font-size: 0.8em;
+
+  position: relative;
   display: flex;
   flex-direction: column;
-  padding: 4px 6px;
-  border: 1px solid grey;
-  border-radius: 6px;
-  position: relative;
-}
-div {
 
+  padding: 8px 12px;
+  border: 1px solid grey;
+  border-radius: 8px;
 }
+
 input {
-  margin: 8px 0;
+  margin-top: 12px;
   padding: 4px 6px;
 }
+
 #title {
   position: absolute;
   top: -1em;
-  padding: .35em;
+
   background-color: #F8FAFB;
+  padding: .35em;
 }
+
+#tips {
+  color: grey;
+  font-size: 0.7em;
+  
+  margin-top: 4px;
+  margin-left: 4px;
+}
+
 </style>
