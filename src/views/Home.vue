080ce238fe12b855ae3490dd5a039bcbfb54d0b5
abdelhaqf <abdel_31354@yahoo.co.id>
2019-05-16 11:23:37 +0700

    optimasi code Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,20 @@
 <template lang="html">
   <div class="home">
+
     <KanbanBoard />
+
   </div>
 </template>
 
 <script>
 import KanbanBoard from '@/components/KanbanBoard.vue'
 import * as dss from 'deepstream.io-client-js'
+
 export default {
   name: 'home',
+
   components: {
     KanbanBoard
-  },
-  created() {
   }
 }
 </script>
-
-<style lang="scss">
-</style>
